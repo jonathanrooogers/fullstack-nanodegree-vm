006730c266fccbc3d612d0b8ce117340eea1e53c
Jonathan <jono-rogers@hotmail.com>
2019-09-23 10:17:31 +1200

    bug fix for imported librairy

--- vagrant/Final Project/Final project.py
+++ vagrant/Final Project/Final project.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+#from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.bind = engine
+#engine = create_engine('sqlite:///restaurantmenu.db')
+#Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+#DBSession = sessionmaker(bind=engine)
+#session = DBSession()
 
 if __name__ == '__main__':
     #app.secret_key= 'super_secret_key'
