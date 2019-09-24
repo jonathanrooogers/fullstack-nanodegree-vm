0d4a1e5f22470ca971c492d6b5f5f9d9d555436c
Jonathan <jono-rogers@hotmail.com>
2019-09-24 12:05:14 +1200

    database setup

--- vagrant/Final Project/Final project.py
+++ vagrant/Final Project/Final project.py
@@ -1,25 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-#from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
-#engine = create_engine('sqlite:///restaurantmenu.db')
-#Base.metadata.bind = engine
-
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #show all restaurants
 @app.route('/')
@@ -43,8 +33,8 @@ def deleteRestaurant():
     return render_template('deleterestaurant.html')
 
 #show menu for restaurant
-@app.route('/restaurant/restaurant_id')
-@app.route('/restaurant/restaurant_id/menu')
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu():
     return render_template('menu.html')
 
